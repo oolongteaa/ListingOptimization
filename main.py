5c2b0203c50bec6c94f91e8656a04b45f6ac5c9e
jstiy <jsti.yeh@gmail.com>
2025-07-04 14:24:58 -0400

    change package and file names to follow naming conventions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from airbnbIntegration import airbnbParser, airbnbScraper
-import poeIntegration
+from airbnb_integration import airbnb_parser, airbnb_scraper
+import poe_integration
 
-POE_API_KEY = "your poe api key here"
+POE_API_KEY = "wcK0nNN_liw1Q1YLfa7GnIjGY-h1mv_0ORnr3c7S6Ms"
 
 def main():
     print("==Listing Optimization App==\n")
@@ -9,17 +9,17 @@ def main():
     room_url = input("Enter the Airbnb room URL: ").strip()
 
     # Fetch and save Airbnb data
-    json_file_path = airbnbScraper.fetch_airbnb_listing(room_url)
+    json_file_path = airbnb_scraper.fetch_airbnb_listing(room_url)
 
     # Parse and write output
     print("Parsing listing details...")
-    parsed_data = airbnbParser.parse_listing_details(json_file_path)
-    airbnbParser.write_output_to_file(parsed_data)
+    parsed_data = airbnb_parser.parse_listing_details(json_file_path)
+    airbnb_parser.write_output_to_file(parsed_data)
     print("Parsed data written to output file.\nDone!")
 
     #  POE bot analysis
     print("Sending info to POE bot...")
-    poeIntegration.run_ota_analysis(
+    poe_integration.run_ota_analysis(
         api_key=POE_API_KEY,
         file_paths=[
             "output.txt",
